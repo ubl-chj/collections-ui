91fb86485b3bfc6a4933c78aacc8653773c426c5
Christopher Johnson <chjohnson39@gmail.com>
2018-06-27 11:17:33 +0200

    refactors full text query interface into new route adds grid to orp adds osd to ubl and orp adds generator to orp

--- src/modules/atomic.js
+++ src/modules/atomic.js
@@ -4,6 +4,7 @@ import '../env'
 import {
   ActionBar,
   ActionBarRow,
+  CheckboxFilter,
   GroupedSelectedFilters,
   HitsStats,
   Layout,
@@ -18,114 +19,66 @@ import {
   SearchkitProvider,
   SideBar,
   SortingSelector,
+  TermQuery,
   TopBar,
   ViewSwitcherHits,
   ViewSwitcherToggle
 } from 'searchkit'
 import '../index.css'
 
-const host = process.env.REACT_APP_ELASTICSEARCH_LOCALHOST + process.env.REACT_APP_ATOMIC_INDEX
+const host = process.env.REACT_APP_ELASTICSEARCH_HOST + process.env.REACT_APP_ATOMIC_INDEX
 const searchkit = new SearchkitManager(host)
 const generatorUrl = process.env.REACT_APP_GENERATOR_BASE
 const osdUrl = process.env.REACT_APP_OSD_BASE
+const viewerUrl = process.env.REACT_APP_UBL_IMAGE_VIEWER_BASE
+const queryFields = ["iiifService", "structureMap.1.@id", "structureMap.1.label", "metadata.Date",
+  "metadata.Title", "metadata.Author", "metadata.Date of publication", "metadata.Call number", "metadata.Collection",
+  "metadata.Objekttitel", "metadata.Part of", "metadata.Place", "metadata.Place of publication", "metadata.Publisher",
+  "metadata.Sprache", "metadata.URN", "metadata.Source PPN (SWB)"]
+const constManifestUrl = generatorUrl + "?type=atomic&index="+ process.env.REACT_APP_ATOMIC_INDEX+ "&q="
 
 const ManifestHitsGridItem = (props) => {
   const {bemBlocks, result} = props
   const source = extend({}, result._source, result.highlight)
   const thumbnail = source.iiifService + "/full/pct:25/0/default.jpg"
-  const url = source.iiifService + "/full/full/0/default.jpg"
+  const url = osdUrl + "?image=" + source.iiifService
   const pathname = new URL(source.iiifService).pathname
   const splitPath = pathname.split("/")
   const viewId = splitPath[5]
-  const viewerUrl = "https://digital.ub.uni-leipzig.de/object/viewid/" + viewId
+  const viewer = viewerUrl + viewId
   const titleString = source.metadata.Title.substr(0, 50) + "... : " + source.imageIndex
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
       <div className={bemBlocks.item("poster")}>
-        <a href={url} target="_blank"><img className={bemBlocks.item("poster")} alt="presentation"
-                                           data-qa="poster" src={thumbnail} width="140"/></a>
+        <a href={url} target="_blank"><img className={bemBlocks.item("poster")} alt="presentation" data-qa="poster"
+          src={thumbnail} width="140"/></a>
       </div>
-      <a href={viewerUrl} target="_blank">
-        <div data-qa="title" className={bemBlocks.item("title")}
-             dangerouslySetInnerHTML={{__html: titleString}}></div>
+      <a href={viewer} target="_blank">
+        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html: titleString}}/>
       </a>
-    </div>
+  </div>
   )
-}
-
-const queryBuilder = (queryString) => {
-  const queryFields = ["iiifService", "structureMap.1.@id", "structureMap.1.label", "metadata.Date", "metadata.Title", "metadata.Author",
-    "metadata.Date of publication", "metadata.Call number", "metadata.Collection", "metadata.Objekttitel", "metadata.Part of",
-    "metadata.Place", "metadata.Place of publication", "metadata.Publisher", "metadata.Sprache", "metadata.URN", "metadata.Source PPN (SWB)"]
-  return {
-    "bool": {
-      "should": [
-        {
-          "nested": {
-            "path": "contentList",
-            "query": {
-              "bool": {
-                "must": [
-                  {"match": {"contentList.chars": queryString}}
-                ],
-              }
-            },
-            "inner_hits": {}
-          }
-        },
-        {
-          "simple_query_string":
-            {
-              "query": queryString,
-              "fields": queryFields
-            }
-        }
-      ]
-    }
-  }
-}
-
-const InnerHits = (innerHits) => {
-  const content = []
-  if (innerHits.contentList != null) {
-    innerHits.contentList.hits.hits.forEach(function(hit) {
-      content.push(hit._source)
-    })
-  }
-  return content
 }
 
 const ManifestsListItem = (props) => {
   const {bemBlocks, result} = props
   const source = extend({}, result._source, result.highlight)
-  const innerHits = extend({}, result.inner_hits)
   const thumbnail = source.iiifService + "/full/90,/0/default.jpg"
-  const url = source.iiifService + "/full/full/0/default.jpg"
+  const url = osdUrl + "?image=" + source.iiifService
   const pathname = new URL(source.iiifService).pathname
   const splitPath = pathname.split("/")
   const viewId = splitPath[5]
-  const viewerUrl = "https://digital.ub.uni-leipzig.de/object/viewid/" + viewId
-  const constManifestUrl = generatorUrl + "?type=atomic&q="
+  const viewer = viewerUrl + viewId
   const query = "{\"query\":{\"multi_match\":{\"query\":\"" + viewId + "\",\"type\":\"cross_fields\",\"operator\":\"and\"}},\"size\":500}"
-  const contentObjs = (
-    <ul className="list-group">
-    {InnerHits(innerHits).map((hit) =>
-      <li className="list-group-item" key={hit.objectId}>
-        <a href={osdUrl + "?image=" + source.iiifService + "&region=" + hit.region} target="_blank">{hit.chars}</a>
-      </li>
-    )}
-  </ul>)
 
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
       <div className={bemBlocks.item("poster")}>
-        <a href={url} target="_blank"><img className="thumbnail" alt="presentation" data-qa="poster"
-                                           src={thumbnail}/></a>
+        <a href={url} target="_blank"><img className="thumbnail" alt="presentation" data-qa="poster" src={thumbnail}/></a>
       </div>
       <div className={bemBlocks.item("details")}>
-        <a href={viewerUrl} target="_blank">
-          <h2 className={bemBlocks.item("title")}
-              dangerouslySetInnerHTML={{__html: source.metadata.Title}}></h2></a>
+        <a href={viewer} target="_blank">
+          <h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html: source.metadata.Title}}/></a>
         <table>
           <tbody>
           <tr>
@@ -139,10 +92,6 @@ const ManifestsListItem = (props) => {
           <tr>
             <td>Date:</td>
             <td>{source.metadata.Date} {source.metadata['Date of publication']} {source.metadata['Datierung']} {source.metadata['datiert']}</td>
-          </tr>
-          <tr>
-            <td>Content Coordinates:</td>
-            <td>{contentObjs}</td>
           </tr>
           <tr>
             <td>Composite Manifest:</td>
@@ -162,56 +111,35 @@ class Atomic extends Component {
         <Layout>
           <TopBar>
             <div className="my-logo">UBL</div>
-            <SearchBox queryBuilder={queryBuilder} autofocus={true} searchOnChange={true}
-            />
+            <SearchBox autofocus={true} searchOnChange={true} queryFields={queryFields}/>
           </TopBar>
-
           <LayoutBody>
             <SideBar>
-              <RefinementListFilter id="tag1" title="Collection"
-                                    field="metadata.Collection.keyword" orderKey="_term"
-                                    operator="AND"/>
+              <RefinementListFilter id="tag1" title="Collection" field="metadata.Collection.keyword" orderKey="_term"
+                operator="AND"/>
             </SideBar>
             <LayoutResults>
               <ActionBar>
                 <ActionBarRow>
-                  <HitsStats translations={{
-                    "hitstats.results_found": "{hitCount} results found"
-                  }}/>
+                  <HitsStats translations={{"hitstats.results_found": "{hitCount} results found"}}/>
                   <ViewSwitcherToggle/>
-                  <SortingSelector options={[
-                    {
-                      label: "Index", key: "index", fields: [
-                        {field: "metadata.Title.keyword", options: {order: "asc"}},
-                        {field: "imageIndex", options: {order: "asc"}}
-                      ]
-                    }
-                  ]}/>
+                  <SortingSelector options={[{label: "Index", key: "index", fields: [
+                    {field: "metadata.Title.keyword", options: {order: "asc"}},
+                      {field: "imageIndex", options: {order: "asc"}}]
+                  }]}/>
+                </ActionBarRow>
+                <ActionBarRow>
+                  <CheckboxFilter id="single-image" title="First Image" label="Select First Image" filter={TermQuery("imageIndex", '00000001')} />
                 </ActionBarRow>
                 <ActionBarRow>
                   <GroupedSelectedFilters/>
                   <ResetFilters/>
                 </ActionBarRow>
               </ActionBar>
-              <ViewSwitcherHits
-                hitsPerPage={50} highlightFields={["metadata.Title"]}
-                sourceFilter={["iiifService", "imageIndex", "metadata.Title", "metadata.Date",
-                  "metadata.Author", "metadata.Date of publication", "metadata.Datierung", "metadata.datiert",
-                  "metadata.Collection", "metadata.Objekttitel", "metadata.Part of",
-                  "metadata.Place", "metadata.Place of publication",
-                  "metadata.Publisher", "metadata.Sprache", "metadata.URN",
-                  "metadata.Source PPN (SWB)"]}
+              <ViewSwitcherHits hitsPerPage={50} highlightFields={["metadata.Title"]}
                 hitComponents={[
-                  {
-                    key: "grid",
-                    title: "Grid",
-                    itemComponent: ManifestHitsGridItem,
-                    defaultOption: true
-                  },
-                  {key: "list", title: "List", itemComponent: ManifestsListItem}
-                ]}
-                scrollTo="body"
-              />
+                  {key: "grid", title: "Grid", itemComponent: ManifestHitsGridItem, defaultOption: true},
+                  {key: "list", title: "List", itemComponent: ManifestsListItem}]} scrollTo="body"/>
               <NoHits suggestionsField={"metadata.Title"}/>
               <Pagination showNumbers={true}/>
             </LayoutResults>
--- src/modules/orp.js
+++ src/modules/orp.js
@@ -1,166 +1,165 @@
 import React, {Component} from 'react'
 import extend from 'lodash/extend'
-import {Col, Container, Row} from 'reactstrap';
 import {
-    ActionBar,
-    ActionBarRow,
-    GroupedSelectedFilters,
-    HitsStats,
-    Layout,
-    LayoutBody,
-    LayoutResults,
-    NoHits,
-    Pagination,
-    RangeFilter,
-    RefinementListFilter,
-    ResetFilters,
-    SearchBox,
-    SearchkitManager,
-    SearchkitProvider,
-    SideBar,
-    SortingSelector,
-    TopBar,
-    ViewSwitcherHits,
-    ViewSwitcherToggle
+  ActionBar,
+  ActionBarRow,
+  GroupedSelectedFilters,
+  HitsStats,
+  Layout,
+  LayoutBody,
+  LayoutResults,
+  NoHits,
+  Pagination,
+  RangeFilter,
+  RefinementListFilter,
+  ResetFilters,
+  SearchBox,
+  SearchkitManager,
+  SearchkitProvider,
+  SideBar,
+  SortingSelector,
+  TopBar,
+  ViewSwitcherHits,
+  ViewSwitcherToggle
 } from 'searchkit'
 import '../index.css'
 
-const host = process.env.REACT_APP_ELASTICSEARCH_HOST + process.env.REACT_APP_ORP_INDEX;
-const searchkit = new SearchkitManager(host);
+const host = process.env.REACT_APP_ELASTICSEARCH_HOST + process.env.REACT_APP_ORP_INDEX
+const searchkit = new SearchkitManager(host)
+const osdUrl = process.env.REACT_APP_OSD_BASE
+const queryFields = ["imageServiceIRI", "metadataMap.tag1", "metadataMap.tag2", "metadataMap.tag3",
+  "metadataMap.tag4", "metadataMap.tag5", "metadataMap.tag6", "metadataMap.tag7", "metadataMap.tag8"]
+const generatorUrl = process.env.REACT_APP_GENERATOR_BASE
+const constManifestUrl = generatorUrl + "?type=orp&index="+ process.env.REACT_APP_ORP_INDEX + "&q="
+
+const ManifestHitsGridItem = (props) => {
+  const {bemBlocks, result} = props
+  const source = extend({}, result._source, result.highlight)
+  const thumbnail = source.imageServiceIRI + "/full/pct:25/0/default.jpg"
+  const url = osdUrl + "?image=" + source.imageServiceIRI
+  const viewer = process.env.REACT_APP_VIEWER_BASE + "/#?c=0&m=0&s=0&cv=0&manifest="
+  let tag4 = source.metadataMap.tag4 || ''
+  let tag5 = source.metadataMap.tag5 || ''
+  let tag6 = source.metadataMap.tag6 || ''
+  let tag7 = source.metadataMap.tag7 || ''
+  let tag8 = source.metadataMap.tag8 || ''
+  const query = getQuery(source.metadataMap.tag3 + " " + tag4 + " " + tag5 + " " + tag6 + " " + tag7 + " " + tag8)
+  const titleString = source.metadataMap.tag3 + " " + source.metadataMap.tag5 + " " + source.metadataMap.tag7 + " "
+    + source.metadataMap.tag8;
+  const finalTitle = titleString.substr(0, 50) + "...: " + source.imageIndex
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <div className={bemBlocks.item("poster")}>
+        <a href={url} target="_blank"><img className={bemBlocks.item("poster")} alt="presentation" data-qa="poster"
+          src={thumbnail} width="180"/></a>
+      </div>
+      <div><a href={viewer + encodeURIComponent(query)}
+        target="_blank"><div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html: finalTitle}}/></a></div>
+    </div>
+  )
+}
 
 const ManifestsListItem = (props) => {
-    const {bemBlocks, result} = props;
-    const source = extend({}, result._source, result.highlight);
-    const manifestUri = process.env.REACT_APP_DYNAMO_BASE + "/dynamo?type=meta&v1=";
-    const viewerIRI = process.env.REACT_APP_VIEWER_BASE + "/#?c=0&m=0&s=0&cv=0&manifest=";
-    const thumbnail = source.imageServiceIRI + "/full/90,/0/default.jpg";
-    const url = source.imageServiceIRI + "/full/full/0/default.jpg";
-    return (
-        <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
-            <div className={bemBlocks.item("poster")}>
-              <a href={url} target="_blank"><img className="thumbnail" alt="presentation" data-qa="poster" src={thumbnail}/></a>
-            </div>
-            <div className={bemBlocks.item("details")}>
-            <Container fluid>
-                <Row>
-                    <Col xs="3">Image:</Col>
-                    <Col xs="auto">{source.imageIndex}</Col>
-                </Row>
-                <Row>
-                    <Col xs="auto">Collection:</Col>
-                    <Col><a
-                        href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag1 + "&v2=")}
-                        target="_blank">{source.metadataMap.tag1}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3">Date Range:</Col>
-                    <Col xs="auto"><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag2 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag2}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3">Date Begin:</Col>
-                    <Col xs="auto"><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag3 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag3}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3"></Col>
-                    <Col><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag4 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag4}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3"></Col>
-                    <Col><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag5 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag5}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3"></Col>
-                    <Col><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag6 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag6}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3"></Col>
-                    <Col><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag7 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag7}</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="3"></Col>
-                    <Col><a href={viewerIRI + encodeURIComponent(manifestUri + source.metadataMap.tag8 + "&v2=")}
-                            target="_blank">{source.metadataMap.tag8}</a>
-                    </Col>
-                </Row>
-            </Container>
-                <hr/>
-            </div>
-        </div>
-    )
+  const {bemBlocks, result} = props
+  const source = extend({}, result._source, result.highlight)
+  const viewerIRI = process.env.REACT_APP_VIEWER_BASE + "/#?c=0&m=0&s=0&cv=0&manifest="
+  const thumbnail = source.imageServiceIRI + "/full/90,/0/default.jpg"
+  const url = osdUrl + "?image=" + source.imageServiceIRI
+  let tag4 = source.metadataMap.tag4 || ''
+  let tag5 = source.metadataMap.tag5 || ''
+  let tag6 = source.metadataMap.tag6 || ''
+  let tag7 = source.metadataMap.tag7 || ''
+  let tag8 = source.metadataMap.tag8 || ''
+  const query = getQuery(source.metadataMap.tag3 + " " + tag4 + " " + tag5 + " " + tag6 + " " + tag7 + " " + tag8)
+
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <div className={bemBlocks.item("poster")}>
+        <a href={url} target="_blank"><img className="thumbnail" alt="presentation" data-qa="poster"
+          src={thumbnail}/></a>
+      </div>
+      <div className={bemBlocks.item("details")}>
+        <table>
+          <tbody>
+          <tr>
+            <td>Image:</td>
+            <td>{source.imageIndex}</td>
+          </tr>
+          <tr>
+            <td>Collection:</td>
+            <td>{source.metadataMap.tag1}</td>
+          </tr>
+          <tr>
+            <td>Date Range:</td>
+            <td>{source.metadataMap.tag2}</td>
+          </tr>
+          <tr>
+            <td>Composite Manifest:</td>
+            <td><a href={viewerIRI + encodeURIComponent(query)}
+              target="_blank">{source.metadataMap.tag3} {source.metadataMap.tag5} {source.metadataMap.tag7} {source.metadataMap.tag8}</a>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <hr/>
+      </div>
+    </div>)
+}
+
+const getQuery = (params) => {
+  const query = JSON.stringify({
+    "query": {
+      "simple_query_string": {
+        "query": params,
+        "default_operator": "and"
+      }
+    }, "size": 500
+  })
+  return constManifestUrl + query
 }
 
 class Orp extends Component {
 
-    render() {
-        return (
-            <SearchkitProvider searchkit={searchkit}>
-                <Layout>
-                    <TopBar>
-                        <div className="my-logo">UBL</div>
-                        <SearchBox autofocus={true} searchOnChange={true}
-                                   queryFields={["metadataMap.tag1", "metadataMap.tag2", "metadataMap.tag3",
-                                       "metadataMap.tag4", "metadataMap.tag5", "metadataMap.tag6",
-                                       " metadataMap.tag7", "metadataMap.tag8"]}
-                        />
-                    </TopBar>
-                    <LayoutBody>
-                        <SideBar>
-                            <RefinementListFilter id="tag1" title="Collection" field="metadataMap.tag1.keyword"
-                                                  orderKey="_term" operator="AND"/>
-                        </SideBar>
-                        <LayoutResults>
-                            <ActionBar>
-
-                                <ActionBarRow>
-                                    <HitsStats translations={{
-                                        "hitstats.results_found": "{hitCount} results found"
-                                    }}/>
-                                    <ViewSwitcherToggle/>
-                                    <SortingSelector options={[
-                                        {label: "Index", field: "imageIndex", order: "asc"},
-                                    ]}/>
-                                </ActionBarRow>
-
-                                <ActionBarRow>
-                                    <GroupedSelectedFilters/>
-                                    <ResetFilters/>
-                                </ActionBarRow>
-
-                            </ActionBar>
-                            <RangeFilter field="metadataMap.tag3.raw" id="date" min={1641} max={1975}
-                                         showHistogram={true} title="Date Selector"/>
-                            <ViewSwitcherHits
-                                hitsPerPage={12} highlightFields={["metadataMap.tag1"]}
-                                sourceFilter={["tag1", "imageIndex", "metadataMap.tag1", "metadataMap.tag2",
-                                    "metadataMap.tag3", "metadataMap.tag4", "metadataMap.tag5", "metadataMap.tag6",
-                                    "metadataMap.tag7", "metadataMap.tag8", "imageServiceIRI"]}
-                                hitComponents={[
-                                    {key: "list", title: "List", itemComponent: ManifestsListItem}
-                                ]}
-                                scrollTo="body"
-                            />
-                            <NoHits suggestionsField={"metadataMap.tag1"}/>
-                            <Pagination showNumbers={true}/>
-                        </LayoutResults>
-
-                    </LayoutBody>
-                </Layout>
-            </SearchkitProvider>
-        )
-    }
+  render () {
+    return (<SearchkitProvider searchkit={searchkit}>
+        <Layout>
+          <TopBar>
+            <div className="my-logo">UBL</div>
+            <SearchBox autofocus={true} searchOnChange={true} queryFields={queryFields}/>
+          </TopBar>
+          <LayoutBody>
+            <SideBar>
+              <RefinementListFilter id="tag1" title="Collection" field="metadataMap.tag1.keyword" orderKey="_term"
+                operator="AND"/>
+            </SideBar>
+            <LayoutResults>
+              <ActionBar>
+                <ActionBarRow>
+                  <HitsStats translations={{
+                    "hitstats.results_found": "{hitCount} results found"
+                  }}/>
+                  <ViewSwitcherToggle/>
+                  <SortingSelector options={[{label: "Index", field: "imageIndex", order: "asc"},]}/>
+                </ActionBarRow>
+                <ActionBarRow>
+                  <GroupedSelectedFilters/>
+                  <ResetFilters/>
+                </ActionBarRow>
+              </ActionBar>
+              <div className="ex1">
+                <RangeFilter field="metadataMap.tag3.raw" id="date" min={1641} max={1975} showHistogram={true}
+                  title="Date Selector"/>
+              </div>
+              <ViewSwitcherHits hitsPerPage={50} highlightFields={["metadataMap.tag1"]}
+                hitComponents={[{key: "list", title: "List", itemComponent: ManifestsListItem},
+                  {key: "grid", title: "Grid", itemComponent: ManifestHitsGridItem}]} scrollTo="body"/>
+              <NoHits suggestionsField={"metadataMap.tag1"}/>
+              <Pagination showNumbers={true}/>
+            </LayoutResults>
+          </LayoutBody>
+        </Layout>
+      </SearchkitProvider>)
+  }
 }
 
 export default Orp
